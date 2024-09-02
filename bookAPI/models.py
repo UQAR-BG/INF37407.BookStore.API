319dd11355f41f80b8178768aeb3d8684ff28ee9
Bastien Goulet <bastien.goulet@uqar.ca>
2024-09-02 11:20:47 -0400

    Added a Book table to orderAPI and created a Many-to-many relationship between Order and Book

--- bookAPI/models.py
+++ bookAPI/models.py
@@ -5,9 +5,12 @@ class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=150)
     author = models.CharField(max_length=80)
-    description = models.CharField(max_length=1000)
+    description = models.CharField(null=True, blank=True, max_length=1000)
     isbn = models.CharField(max_length=13, help_text='Please supply a 13 digit-long ISBN.')
     price = models.DecimalField(max_digits=5, decimal_places=2)
     stock = models.IntegerField(default=0)
     published_date = models.DateField()
     genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return "{}, Author: {}, ISBN: {}".format(self.title, self.author, self.isbn)
--- orderAPI/models.py
+++ orderAPI/models.py
@@ -4,6 +4,9 @@ class User(models.Model):
     id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length = 254)
 
+    def __str__(self):
+        return self.email
+
 class Order(models.Model):
     id = models.AutoField(primary_key=True)
     order_date = models.DateTimeField(auto_now_add=True)
@@ -12,9 +15,19 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     made_by_user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class BookLine(models.Model):
+class Book(models.Model):
     id = models.AutoField(primary_key=True)
     isbn = models.CharField(max_length=13, help_text='Please supply a 13 digit-long ISBN.')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    orders = models.ManyToManyField(Order, through="BookLine")
+
+    def __str__(self):
+        return self.isbn
+
+class BookLine(models.Model):
+    id = models.AutoField(primary_key=True)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    placed_in_order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    related_to_book = models.ForeignKey(Book, on_delete=models.CASCADE, null=True)
+    placed_in_order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
